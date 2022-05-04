fd4369c5e5dbd69e35b400c67c7ab103f3441376
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-05-04 09:12:32 +0100

    Changed the logic of the app.

--- app.js
+++ app.js
@@ -1,87 +1,48 @@
 const outputDisplay = document.getElementById("output-display");
-const form = document.querySelector("form");
 const btns = document.querySelectorAll(".btn");
-let currNumber,
-  currOperation = "",
-  firstNumber = [],
-  isInModifiedState = false,
-  operation = null,
-  secondNumber = [];
+
+let currOperation = "",
+  resultDisplayed = false,
+  result = "";
+
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     if (btn.value !== "") {
+      resultDisplayed = false;
       currOperation += btn.value;
-      console.log(btn.value);
     }
-    currNumber = parseInt(e.target.value);
-    // If the button is a number or a dot
-    if (isFinite(currNumber) || e.target.value === ".")
-      isInModifiedState
-        ? secondNumber.push(currNumber || e.target.value)
-        : firstNumber.push(currNumber || e.target.value);
     //? DELETE BUTTON
     if (e.target.textContent === "DEL") {
-      `${isInModifiedState ? secondNumber.pop() : firstNumber.pop()}`;
-    }
-    //? ADD BUTTON
-    if (e.target.textContent === "+") {
-      outputDisplay.value = firstNumber.join("");
-      isInModifiedState = true;
-      operation = "+";
-    }
-    //? SUBTRACT BUTTON
-    if (e.target.textContent === "-") {
-      isInModifiedState = true;
-      operation = "-";
-    }
-    //? MULTIPLY BUTTON
-    if (e.target.textContent === "x") {
-      isInModifiedState = true;
-      operation = "x";
-    }
-    //? DIVIDE BUTTON
-    if (e.target.textContent === "/") {
-      isInModifiedState = true;
-      operation = "/";
+      resultDisplayed = false;
+      currOperation = currOperation.slice(0, -1);
     }
     //? EQUALS BUTTON
     if (e.target.textContent === "=") {
-      isInModifiedState = !isInModifiedState;
-      let result;
-      if (operation === "+") {
-        result =
-          parseFloat(firstNumber.join("")) + parseFloat(secondNumber.join(""));
-      } else if (operation === "-") {
-        result =
-          parseFloat(firstNumber.join("")) - parseFloat(secondNumber.join(""));
-      } else if (operation === "x") {
-        result =
-          parseFloat(firstNumber.join("")) * parseFloat(secondNumber.join(""));
-      } else if (operation === "/") {
-        result =
-          parseFloat(firstNumber.join("")) / parseFloat(secondNumber.join(""));
+      resultDisplayed = true;
+      try {
+        currOperation = result = String(eval(currOperation.replace(/×/g, "*")));
+      } catch {
+        result = "Invalid Input";
+        currOperation = "";
       }
-      outputDisplay.value = result;
-      firstNumber = result ? [...result.toString()] : [];
-      secondNumber = [];
       e.preventDefault();
-      return;
     }
-
     //? RESET BUTTON
     if (e.target.type === "reset") {
-      isInModifiedState = false;
-      firstNumber = [];
-      secondNumber = [];
+      resultDisplayed = false;
+      currOperation = "";
     }
-    if (isInModifiedState) {
-      if (secondNumber.length >= 1) {
-        outputDisplay.value = secondNumber.join("");
-      }
-    } else {
-      outputDisplay.value = firstNumber.join("");
-    }
+
+    outputDisplay.value = resultDisplayed ? result : currOperation;
     e.preventDefault();
-    console.log(currOperation);
   });
 });
+
+/* function formatOutput(str) {
+  let output = [];
+  for (let i = 0; i < str.length; i += 3) {
+    output.push(str.substring(i, i + 3));
+  }
+  return output.join();
+}
+ */
